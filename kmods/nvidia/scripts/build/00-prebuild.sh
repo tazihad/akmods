8a63b9e37d2305be43de13d4af79c209b30db79c
Zihad <zihad.ta@gmail.com>
2024-09-01 18:43:54 +0600

    Update 00-prebuild.sh

--- kmods/nvidia/scripts/build/00-prebuild.sh
+++ kmods/nvidia/scripts/build/00-prebuild.sh
@@ -2,11 +2,11 @@
 
 set -oeux pipefail
 
-dnf install \
+dnf install -y \
   "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
   "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 
-dnf update \
+dnf update -y \
   --uninstall rpmfusion-free-release \
   --uninstall rpmfusion-nonfree-release \
   --install rpmfusion-free-release \

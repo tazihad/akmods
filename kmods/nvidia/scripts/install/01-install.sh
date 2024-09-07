e696a53a81e3f9712aaefb801f6c52b19f767d71
Zihad <zihad.ta@gmail.com>
2024-09-07 21:28:13 +0600

    fix install.sh

--- kmods/nvidia/scripts/install/01-install.sh
+++ kmods/nvidia/scripts/install/01-install.sh
@@ -10,6 +10,6 @@ sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/nvidia-container-toolkit.repo
 
 # Install Nvidia drivers
 rpm-ostree install \
-    xorg-x11-drv-nvidia-{,cuda-,devel-,kmodsrc-,power-}${NVIDIA_FULL_VERSION} \
-    nvidia-container-toolkit nvidia-vaapi-driver \
-    /tmp/akmods/rpms/kmod-nvidia-${KERNEL_VERSION}-${NVIDIA_AKMOD_VERSION}.fc${RELEASE}.rpm
+  xorg-x11-drv-nvidia-{,cuda-,devel-,kmodsrc-,power-}${NVIDIA_FULL_VERSION} \
+  nvidia-container-toolkit nvidia-vaapi-driver \
+  /tmp/akmods/rpms/kmod-nvidia-${KERNEL_VERSION}-${NVIDIA_AKMOD_VERSION}.fc${RELEASE}.rpm

6e37486ebcf1fbca907542bf6ad10b8bc33167e5
Zihad <zihad.ta@gmail.com>
2024-09-07 21:48:12 +0600

    prebuild fix

--- kmods/nvidia/scripts/build/00-prebuild.sh
+++ kmods/nvidia/scripts/build/00-prebuild.sh
@@ -2,12 +2,6 @@
 
 set -oeux pipefail
 
-# Disable repos that are not needed for the build to improve build times
-if [[ "${FEDORA_VERSION}" -lt 39 ]]; then
-  sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-{cisco-openh264,updates-modular}.repo
-else
-  sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
-fi
 
 rpm-ostree install \
   "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
@@ -18,10 +12,3 @@ rpm-ostree install \
   rpmfusion-nonfree-release \
   --uninstall rpmfusion-free-release-$(rpm -E %fedora)-1.noarch \
   --uninstall rpmfusion-nonfree-release-$(rpm -E %fedora)-1.noarch
-
-# If RPMFUSION_TESTING_ENABLED is set to true, enable the RPMFusion testing repos
-if [[ "${RPMFUSION_TESTING_ENABLED}" == "true" ]]; then
-  sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}-updates-testing.repo
-fi
-
-sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/fedora-updates-archive.repo

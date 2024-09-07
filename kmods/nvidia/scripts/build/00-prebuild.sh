af907831a0ab127c71454ae536775f7fd8234dac
Zihad <zihad.ta@gmail.com>
2024-09-07 21:44:05 +0600

    improve scripts

--- kmods/nvidia/scripts/build/00-prebuild.sh
+++ kmods/nvidia/scripts/build/00-prebuild.sh
@@ -2,6 +2,13 @@
 
 set -oeux pipefail
 
+# Disable repos that are not needed for the build to improve build times
+if [[ "${FEDORA_VERSION}" -lt 39 ]]; then
+  sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-{cisco-openh264,updates-modular}.repo
+else
+  sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
+fi
+
 rpm-ostree install \
   "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
   "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
@@ -12,9 +19,9 @@ rpm-ostree install \
   --uninstall rpmfusion-free-release-$(rpm -E %fedora)-1.noarch \
   --uninstall rpmfusion-nonfree-release-$(rpm -E %fedora)-1.noarch
 
-# If REPOSITORY_TYPE is set to true, enable the RPMFusion testing repos
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}-updates-testing.repo
-if [[ "${REPOSITORY_TYPE}" == "testing" ]]; then
-  echo "INFO: Enabling RPMFusion testing repositories"
+# If RPMFUSION_TESTING_ENABLED is set to true, enable the RPMFusion testing repos
+if [[ "${RPMFUSION_TESTING_ENABLED}" == "true" ]]; then
   sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}-updates-testing.repo
 fi
+
+sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/fedora-updates-archive.repo
--- kmods/nvidia/scripts/install/00-preinstall.sh
+++ kmods/nvidia/scripts/install/00-preinstall.sh
@@ -7,7 +7,7 @@ source /tmp/akmods/info/nvidia-vars
 cp -a /etc/yum.repos.d /tmp/yum.repos.d
 
 # Configure rpmfusion repos
-sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/fedora-{cisco-openh264,updates-archive}.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-{cisco-openh264,updates-archive}.repo
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}{,-updates}.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}-updates-testing.repo
 if [[ "${REPOSITORY_TYPE}" == "testing" ]]; then

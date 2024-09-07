83f624f911189367b86cb0f9244ffcdc2e2e53ba
Zihad <zihad.ta@gmail.com>
2024-09-07 21:23:00 +0600

    fix build.sh

--- kmods/nvidia/scripts/build/01-build.sh
+++ kmods/nvidia/scripts/build/01-build.sh
@@ -4,23 +4,19 @@ set -oeux pipefail
 
 RELEASE="$(rpm -E '%fedora.%_arch')"
 
+
 # Build NVIDIA drivers
 rpm-ostree install \
-    akmod-nvidia*:${KMOD_VERSION}.*.fc${RELEASE} \
-    xorg-x11-drv-nvidia-{,cuda,devel,kmodsrc,power}*:${KMOD_VERSION}.*.fc${RELEASE}
+    akmod-nvidia-*:${KMOD_VERSION}.*.fc${RELEASE}
 
 KERNEL_VERSION="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 NVIDIA_AKMOD_VERSION="$(basename "$(rpm -q "akmod-nvidia" --queryformat '%{VERSION}-%{RELEASE}')" ".fc${RELEASE%%.*}")"
-NVIDIA_LIB_VERSION="$(basename "$(rpm -q "xorg-x11-drv-nvidia" --queryformat '%{VERSION}-%{RELEASE}')" ".fc${RELEASE%%.*}")"
-NVIDIA_FULL_VERSION="$(rpm -q "xorg-x11-drv-nvidia" --queryformat '%{EPOCH}:%{VERSION}-%{RELEASE}.%{ARCH}')"
 
 akmods --force --kernels "${KERNEL_VERSION}" --kmod "nvidia"
 
 modinfo /usr/lib/modules/${KERNEL_VERSION}/extra/nvidia/nvidia{,-drm,-modeset,-peermem,-uvm}.ko.xz > /dev/null || \
 (cat /var/cache/akmods/nvidia/${NVIDIA_AKMOD_VERSION}-for-${KERNEL_VERSION}.failed.log && exit 1)
 
-# View license information
-modinfo -l /usr/lib/modules/${KERNEL_VERSION}/extra/nvidia/nvidia{,-drm,-modeset,-peermem,-uvm}.ko.xz
 
 # Build nvidia-addons
 ADDONS_DIR="/tmp/rpm-specs/nvidia-addons"
@@ -28,6 +24,7 @@ mkdir -p ${ADDONS_DIR}/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS,tmp}
 curl -Lo ${ADDONS_DIR}/rpmbuild/SOURCES/nvidia-container-toolkit.repo https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo
 curl -Lo ${ADDONS_DIR}/rpmbuild/SOURCES/nvidia-container.pp https://raw.githubusercontent.com/NVIDIA/dgx-selinux/master/bin/RHEL9/nvidia-container.pp
 mv /tmp/files/* ${ADDONS_DIR}/rpmbuild/SOURCES/
+cp /etc/yum.repos.d/negativo17-nvidia.repo ${ADDONS_DIR}/rpmbuild/SOURCES/negativo17-nvidia.repo
 
 sed -i "s@gpgcheck=0@gpgcheck=1@" ${ADDONS_DIR}/rpmbuild/SOURCES/nvidia-container-toolkit.repo
 
@@ -48,8 +45,6 @@ KERNEL_VERSION=${KERNEL_VERSION}
 RELEASE=${RELEASE}
 NVIDIA_PACKAGE_NAME=nvidia
 NVIDIA_MAJOR_VERSION=${KMOD_VERSION}
-NVIDIA_FULL_VERSION=${NVIDIA_FULL_VERSION}
 NVIDIA_AKMOD_VERSION=${NVIDIA_AKMOD_VERSION}
-NVIDIA_LIB_VERSION=${NVIDIA_LIB_VERSION}
 REPOSITORY_TYPE=${REPOSITORY_TYPE}
 EOF
